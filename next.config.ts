110bf5c0cab0a43e8f057a7a19dbd62f91bd75e5
benaya josua <benaya.josua@gmail.com>
2026-06-02 18:39:31 +0700

    update : link cheatsheet

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,12 @@ const nextConfig: NextConfig = {
         destination:
           "https://drive.google.com/drive/folders/1NIQmYw2XwMp7zCTQFc2J3N-9i9hg-13L?usp=sharing",
         permanent: false,
+      },
+      {
+        source: "/cheatsheet-wdk3",
+        destination:
+          "https://uph365-my.sharepoint.com/:f:/g/personal/01082240013_student_uph_edu/IgAuktWUK_-JS6Icr7tFbhQ-AWaMU_5Zsc0vTlnBIbqG1zs?e=zakJau",
+        permanent: false,
       }
     ];
   },
